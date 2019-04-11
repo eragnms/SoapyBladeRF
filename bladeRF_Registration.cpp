954b6ea874bc6385f240274e3d726d83ce53f240
mats <mats@mcnab.se>
2019-04-11 08:34:22 +0200

    indenting some of the files

--- bladeRF_Registration.cpp
+++ bladeRF_Registration.cpp
@@ -28,90 +28,90 @@
 
 static SoapySDR::Kwargs devinfo_to_kwargs(const bladerf_devinfo &info)
 {
-    SoapySDR::Kwargs args;
+        SoapySDR::Kwargs args;
 
-    args["backend"] = bladerf_backend_str(info.backend);
-    args["serial"] = std::string(info.serial);
+        args["backend"] = bladerf_backend_str(info.backend);
+        args["serial"] = std::string(info.serial);
 
-    char buff[100];
-    int r = std::sprintf(buff, "0x%02X:0x%02X", int(info.usb_bus), int(info.usb_addr));
-    if (r > 0) args["device"] = std::string(buff, r);
+        char buff[100];
+        int r = std::sprintf(buff, "0x%02X:0x%02X", int(info.usb_bus), int(info.usb_addr));
+        if (r > 0) args["device"] = std::string(buff, r);
 
-    r = std::sprintf(buff, "%u", info.instance);
-    if (r > 0) args["instance"] = std::string(buff, r);
+        r = std::sprintf(buff, "%u", info.instance);
+        if (r > 0) args["instance"] = std::string(buff, r);
 
-    std::string shortSerial(std::string(info.serial));
-    //serial can take the value "ANY" on permissions errors
-    if (shortSerial.size() > 20) shortSerial.replace(8, 16, "..");
-    args["label"] = "BladeRF #" + args["instance"] + " [" + shortSerial + "]";
+        std::string shortSerial(std::string(info.serial));
+        //serial can take the value "ANY" on permissions errors
+        if (shortSerial.size() > 20) shortSerial.replace(8, 16, "..");
+        args["label"] = "BladeRF #" + args["instance"] + " [" + shortSerial + "]";
 
-    return args;
+        return args;
 }
 
 static bladerf_devinfo kwargs_to_devinfo(const SoapySDR::Kwargs &args)
 {
-    std::stringstream ss;
+        std::stringstream ss;
 
-    if (args.count("backend") != 0)
-    {
-        ss << args.at("backend") << ":";
-    }
-    else ss << "*:";
+        if (args.count("backend") != 0)
+        {
+                ss << args.at("backend") << ":";
+        }
+        else ss << "*:";
 
-    if (args.count("device") != 0)
-    {
-        ss << "device=" << args.at("device") << " ";
-    }
+        if (args.count("device") != 0)
+        {
+                ss << "device=" << args.at("device") << " ";
+        }
 
-    if (args.count("instance") != 0)
-    {
-        ss << "instance=" << args.at("instance") << " ";
-    }
+        if (args.count("instance") != 0)
+        {
+                ss << "instance=" << args.at("instance") << " ";
+        }
 
-    if (args.count("serial") != 0)
-    {
-        ss << "serial=" << args.at("serial") << " ";
-    }
+        if (args.count("serial") != 0)
+        {
+                ss << "serial=" << args.at("serial") << " ";
+        }
 
-    bladerf_devinfo info;
-    bladerf_init_devinfo(&info);
-    bladerf_get_devinfo_from_str(ss.str().c_str(), &info);
-    return info;
+        bladerf_devinfo info;
+        bladerf_init_devinfo(&info);
+        bladerf_get_devinfo_from_str(ss.str().c_str(), &info);
+        return info;
 }
 
 static std::vector<SoapySDR::Kwargs> find_bladeRF(const SoapySDR::Kwargs &matchArgs)
 {
-    const bladerf_devinfo matchinfo = kwargs_to_devinfo(matchArgs);
+        const bladerf_devinfo matchinfo = kwargs_to_devinfo(matchArgs);
 
-    std::vector<SoapySDR::Kwargs> results;
-    bladerf_devinfo *infos = NULL;
-    int ret = 0;
-    ret = bladerf_get_device_list(&infos);
+        std::vector<SoapySDR::Kwargs> results;
+        bladerf_devinfo *infos = NULL;
+        int ret = 0;
+        ret = bladerf_get_device_list(&infos);
 
-    for (int i = 0; i < ret; i++)
-    {
-        if (bladerf_devinfo_matches(infos+i, &matchinfo))
+        for (int i = 0; i < ret; i++)
         {
-            results.push_back(devinfo_to_kwargs(infos[i]));
+                if (bladerf_devinfo_matches(infos+i, &matchinfo))
+                {
+                        results.push_back(devinfo_to_kwargs(infos[i]));
+                }
         }
-    }
 
-    bladerf_free_device_list(infos);
-    return results;
+        bladerf_free_device_list(infos);
+        return results;
 }
 
 static SoapySDR::Device *make_bladeRF(const SoapySDR::Kwargs &args)
 {
-    SoapySDR::Device *bladerf = new bladeRF_SoapySDR(kwargs_to_devinfo(args));
+        SoapySDR::Device *bladerf = new bladeRF_SoapySDR(kwargs_to_devinfo(args));
 
-    //apply applicable settings found in args
-    for (const auto &info : bladerf->getSettingInfo())
-    {
-        if (args.count(info.key) == 0) continue;
-        bladerf->writeSetting(info.key, args.at(info.key));
-    }
+        //apply applicable settings found in args
+        for (const auto &info : bladerf->getSettingInfo())
+        {
+                if (args.count(info.key) == 0) continue;
+                bladerf->writeSetting(info.key, args.at(info.key));
+        }
 
-    return bladerf;
+        return bladerf;
 }
 
 static SoapySDR::Registry register__bladeRF("bladerf", &find_bladeRF, &make_bladeRF, SOAPY_SDR_ABI_VERSION);
